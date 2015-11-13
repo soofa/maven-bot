ddca1055e45ddc3b9c0f1aa5c1ddf4fb40aadfe8
Bob Breznak <bob.breznak@gmail.com>
2015-11-13 11:32:51 -0500

    Add Forcast plugin

--- Gemfile
+++ Gemfile
@@ -3,5 +3,7 @@ source 'https://rubygems.org'
 gem 'lita'
 gem 'lita-slack'
 
+gem 'lita-forecast'
+
 # gem "lita-google-images"
 # gem "lita-karma"
--- lita_config.rb
+++ lita_config.rb
@@ -14,25 +14,13 @@ Lita.configure do |config|
   # Messages at the selected level and above will be logged.
   config.robot.log_level = :info
 
-  # An array of user IDs that are considered administrators. These users
-  # the ability to add and remove other users from authorization groups.
-  # What is considered a user ID will change depending on which adapter you use.
-  # config.robot.admins = ["1", "2"]
-
   # The adapter you want to connect with. Make sure you've added the
   # appropriate gem to the Gemfile.
   config.robot.adapter = :slack
-  #config.robot.admins = ["U012A3BCD"]
+  config.robot.admins = ['U040NEHU5']
   config.adapters.slack.token = ENV['SLACK_API_TOKEN']
-  
 
-  ## Example: Set options for the chosen adapter.
-  # config.adapter.username = "myname"
-  # config.adapter.password = "secret"
-
-  ## Example: Set options for the Redis connection.
-  # config.redis.host = "127.0.0.1"
-  # config.redis.port = 1234
+  config.handlers.forecast.api_key = ENV['FORECAST_API_TOKEN']
 
   ## Example: Set configuration for any loaded handlers. See the handler's
   ## documentation for options.
